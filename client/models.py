1d94c3e4ca11849f0d4eba5744421e6a90915762
Dmitry <dimasir00@yandex.ru>
2018-03-15 15:33:03 +0700

    user model & logotypes

--- client/models.py
+++ client/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.IntegerField(default=0, blank=False)
+    oil = models.IntegerField(default=0, blank=False)
+    stat_tower = models.IntegerField(default=0, blank=False)
+    stat_produced = models.IntegerField(default=0, blank=False)
+    stat_pay = models.IntegerField(default=0, blank=False)
+    stat_payout = models.IntegerField(default=0, blank=False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
