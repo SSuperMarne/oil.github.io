5357a912461e0a3e8875096f0aca87011e4a0f76
Dmitry <dimasir00@yandex.ru>
2018-03-22 23:24:34 +0700

    Remove extra import

--- game/views.py
+++ game/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
