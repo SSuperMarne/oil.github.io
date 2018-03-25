0b97400badb618098546a925f45e9e392d31856b
Dmitry <dimasir00@yandex.ru>
2018-03-25 22:35:56 +0700

    PAYEER module reconfigured

--- payment/forms.py
+++ payment/forms.py
@@ -6,5 +6,4 @@ STATUS_CHOICES = (
 
 class ReplenishForm(forms.Form):
     amount = forms.IntegerField(min_value=1)
-    desc = forms.CharField(min_length=25, max_length=100)
     system = forms.ChoiceField(choices = STATUS_CHOICES)
--- payment/views.py
+++ payment/views.py
@@ -11,30 +11,38 @@ from game.models import Statistic
 import base64
 import hashlib
 
+# configuration
+PAYEER_SHOP_ID = "518803834"
+PAYEER_CURR = "RUB"
+PAYEER_SECRET = "Qt8rv6hCT379QQduzSJnCVBC3WePv3pT"
+
 @login_required
 def add_payment(request):
     if request.method == 'POST':
         form = ReplenishForm(request.POST)
         if form.is_valid():
-            m_amount = form.cleaned_data.get('m_amount')
-            description = form.cleaned_data.get('m_desc')
-            m_shop = "518803834" # ID магазина
-            m_curr = "RUB" # валюта
-            m_desc = base64.b64encode(description.encode('utf-8')).decode('utf-8')
-            m_key = "Qt8rv6hCT379QQduzSJnCVBC3WePv3pT" # секретный ключ
-            order = Order(amount=m_amount, user_id=request.user.id, status=False)
-            order.save()
-            m_orderid = order.id
-            # требование Payeer добавлять .00 у суммы
-            m_amount = str(form.cleaned_data.get('m_amount')) + ".00"
-            # создание подписи #
-            list_of_value_for_sign = map(str, [m_shop, m_orderid, m_amount, m_curr, m_desc, m_key])
-            result_string = ":".join(list_of_value_for_sign).encode()
-            sign_hash = hashlib.sha256(result_string)
-            sign = sign_hash.hexdigest().upper()
-            # конец создания подписи #
-            context = [{'m_shop': m_shop, 'm_orderid': m_orderid, 'm_amount': m_amount, 'm_curr': m_curr, 'm_desc': m_desc, 'm_sign': sign}]
-            return render(request, 'payment/process.html', {'context': context})
+            amount = form.cleaned_data.get('amount')
+            system = form.cleaned_data.get('system')
+            description = "Пополнение баланса пользователя %s" % request.user.username
+            # Free-Kassa
+            if system == "1":
+                pass
+            # Payeer
+            elif system == "2":
+                m_desc = base64.b64encode(description.encode('utf-8')).decode('utf-8')
+                order = Order(amount=amount, user_id=request.user.id, status=False)
+                order.save()
+                m_orderid = order.id
+                # требование Payeer добавлять .00 у суммы
+                m_amount = str(amount) + ".00"
+                # создание подписи #
+                list_of_value_for_sign = map(str, [PAYEER_SHOP_ID, m_orderid, m_amount, PAYEER_CURR, m_desc, PAYEER_SECRET])
+                result_string = ":".join(list_of_value_for_sign).encode()
+                sign_hash = hashlib.sha256(result_string)
+                sign = sign_hash.hexdigest().upper()
+                # конец создания подписи #
+                context = [{'m_shop': PAYEER_SHOP_ID, 'm_orderid': m_orderid, 'm_amount': m_amount, 'm_curr': PAYEER_CURR, 'm_desc': m_desc, 'm_sign': sign}]
+                return render(request, 'payment/process_payeer.html', {'context': context})
         else:
             messages.add_message(request, messages.ERROR, "Введено неверное значение. Действие отменено.")
     return render(request, 'main/add_payment.html')
@@ -42,11 +50,10 @@ def add_payment(request):
 @csrf_exempt
 def payeer_status(request):
     if request.method == 'POST' and 'm_operation_id' in request.POST and 'm_sign' in request.POST:
-        m_key = "Qt8rv6hCT379QQduzSJnCVBC3WePv3pT" # секретный ключ
         list_of_value_for_sign = map(str, [request.POST['m_operation_id'], request.POST['m_operation_ps'], 
         request.POST['m_operation_date'], request.POST['m_operation_pay_date'], request.POST['m_shop'], 
         request.POST['m_orderid'], request.POST['m_amount'], request.POST['m_curr'], 
-        request.POST['m_desc'], request.POST['m_status'], m_key])
+        request.POST['m_desc'], request.POST['m_status'], PAYEER_SECRET])
         result_string = ":".join(list_of_value_for_sign).encode()
         sign_hash = hashlib.sha256(result_string)
         sign = sign_hash.hexdigest().upper()
