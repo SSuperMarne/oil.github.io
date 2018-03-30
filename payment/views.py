e2e454d005dee63053f4cac16f65a9ca4905bf04
Dmitry <dimasir00@yandex.ru>
2018-03-31 03:49:48 +0700

    Payment code optimize

--- payment/views.py
+++ payment/views.py
@@ -41,6 +41,30 @@ def add_balance(order, success):
     r_referrer.save()
     return HttpResponse(str(success))
 
+def create_pay_sign(request, amount, system, m_orderid):
+    description = "Пополнение баланса пользователя %s" % request.user.username
+    # Free-Kassa
+    if system == "1":
+        # создание подписи #
+        list_for_sign = map(str, [FREEKASSA_SHOP_ID, amount, FREEKASSA_SECRET, m_orderid])
+        result_string = ":".join(list_for_sign).encode()
+        sign_hash = hashlib.md5(result_string)
+        sign = sign_hash.hexdigest()
+        # конец создания подписи #
+        return redirect("https://www.free-kassa.ru/merchant/cash.php?m={0}&oa={1}&o={2}&s={3}&em={4}".format(FREEKASSA_SHOP_ID, amount, m_orderid, sign, request.user.email))
+    # Payeer
+    elif system == "2":
+        m_desc = base64.b64encode(description.encode('utf-8')).decode('utf-8')
+        m_amount = str(amount) + ".00" # требование Payeer
+        # создание подписи #
+        list_of_value_for_sign = map(str, [PAYEER_SHOP_ID, m_orderid, m_amount, PAYEER_CURR, m_desc, PAYEER_SECRET])
+        result_string = ":".join(list_of_value_for_sign).encode()
+        sign_hash = hashlib.sha256(result_string)
+        sign = sign_hash.hexdigest().upper()
+        # конец создания подписи #
+        context = [{'m_shop': PAYEER_SHOP_ID, 'm_orderid': m_orderid, 'm_amount': m_amount, 'm_curr': PAYEER_CURR, 'm_desc': m_desc, 'm_sign': sign}]
+        return render(request, 'payment/process_payeer.html', {'context': context})
+
 @login_required
 def add_payment(request):
     if request.method == 'POST':
@@ -48,32 +72,10 @@ def add_payment(request):
         if form.is_valid():
             amount = form.cleaned_data.get('amount')
             system = form.cleaned_data.get('system')
-            description = "Пополнение баланса пользователя %s" % request.user.username
             # Создание платежа
             order = Order(amount=amount, user_id=request.user.id, status=False)
             order.save()
-            m_orderid = order.id
-            # Free-Kassa
-            if system == "1":
-                # создание подписи #
-                list_for_sign = map(str, [FREEKASSA_SHOP_ID, amount, FREEKASSA_SECRET, m_orderid])
-                result_string = ":".join(list_for_sign).encode()
-                sign_hash = hashlib.md5(result_string)
-                sign = sign_hash.hexdigest()
-                # конец создания подписи #
-                return redirect("https://www.free-kassa.ru/merchant/cash.php?m={0}&oa={1}&o={2}&s={3}&em={4}".format(FREEKASSA_SHOP_ID, amount, m_orderid, sign, request.user.email))
-            # Payeer
-            elif system == "2":
-                m_desc = base64.b64encode(description.encode('utf-8')).decode('utf-8')
-                m_amount = str(amount) + ".00" # требование Payeer
-                # создание подписи #
-                list_of_value_for_sign = map(str, [PAYEER_SHOP_ID, m_orderid, m_amount, PAYEER_CURR, m_desc, PAYEER_SECRET])
-                result_string = ":".join(list_of_value_for_sign).encode()
-                sign_hash = hashlib.sha256(result_string)
-                sign = sign_hash.hexdigest().upper()
-                # конец создания подписи #
-                context = [{'m_shop': PAYEER_SHOP_ID, 'm_orderid': m_orderid, 'm_amount': m_amount, 'm_curr': PAYEER_CURR, 'm_desc': m_desc, 'm_sign': sign}]
-                return render(request, 'payment/process_payeer.html', {'context': context})
+            return create_pay_sign(request, amount, system, order.id)
         else:
             messages.add_message(request, messages.ERROR, "Введено неверное значение. Действие отменено.")
     return render(request, 'main/add_payment.html')
